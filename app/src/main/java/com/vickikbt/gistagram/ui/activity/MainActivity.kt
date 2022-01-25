b121a7cc8d275222ab508e1471b78d5685f159be
Victor Kabata <39780120+VictorKabata@users.noreply.github.com>
2022-01-25 18:57:03 +0300

    feat: Setup app splash screen

--- app/src/main/java/com/vickikbt/gistagram/ui/activity/MainActivity.kt
+++ app/src/main/java/com/vickikbt/gistagram/ui/activity/MainActivity.kt
@@ -5,18 +5,19 @@ import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.compose.material.MaterialTheme
 import androidx.compose.material.Surface
-import androidx.compose.material.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.core.splashscreen.SplashScreen.Companion.installSplashScreen
 import com.vickikbt.gistagram.ui.theme.GistagramTheme
 
 class MainActivity : ComponentActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+        installSplashScreen()
         setContent {
             GistagramTheme {
-                Surface(color = MaterialTheme.colors.surface) {
+                Surface(color = MaterialTheme.colors.background) {
                     MainScreen()
                 }
             }
