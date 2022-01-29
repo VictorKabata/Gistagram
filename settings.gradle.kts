1dae53fc0968e433a9f5611aa28812b037399b47
Victor Kabata <39780120+VictorKabata@users.noreply.github.com>
2022-01-30 02:00:09 +0300

    feat: Migrated settings.gradle to dsl

--- settings.gradle.kts
+++ settings.gradle.kts
@@ -3,9 +3,9 @@ dependencyResolutionManagement {
     repositories {
         google()
         mavenCentral()
-        maven { url 'https://jitpack.io' }
+        maven { url = uri("https://jitpack.io") }
     }
 }
 rootProject.name = "Gistagram"
-include ':app'
-include ':shared'
+
+include(":app", ":shared")
