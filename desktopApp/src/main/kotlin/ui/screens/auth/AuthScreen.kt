e39f5d86eac3198b8318fd71d7fb2e6761e9a23f
Victor Kabata <39780120+VictorKabata@users.noreply.github.com>
2022-07-07 21:31:33 +0300

    Setup auth screen

--- desktopApp/src/main/kotlin/ui/screens/auth/AuthScreen.kt
+++ desktopApp/src/main/kotlin/ui/screens/auth/AuthScreen.kt
@@ -1,8 +1,65 @@
 package ui.screens.auth
 
+import androidx.compose.foundation.layout.*
+import androidx.compose.material.Button
+import androidx.compose.material.Icon
+import androidx.compose.material.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.text.font.FontWeight
+import androidx.compose.ui.text.style.TextAlign
+import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
+import java.awt.Desktop
+import java.net.URI
 
 @Composable
-fun AuthScreen(){
+fun AuthScreen() {
+    Box(modifier = Modifier.fillMaxSize()) {
+        Icon(
+            modifier = Modifier.size(200.dp).align(Alignment.Center),
+            painter = painterResource("ic_logo.png"),
+            contentDescription = "Logo"
+        )
 
+        Column(
+            modifier = Modifier.fillMaxWidth().align(Alignment.BottomCenter),
+            horizontalAlignment = Alignment.CenterHorizontally
+        ) {
+            Button(
+                onClick = {
+                    Desktop.getDesktop().browse(URI("https://github.com/VictorKabata"))
+                    //ToDo: Open browser for Oauth flow
+                },
+            ) {
+                Text(
+                    modifier = Modifier.padding(horizontal = 88.dp),
+                    text = "LOGIN",
+                    fontSize = 24.sp,
+                    fontWeight = FontWeight.SemiBold,
+                    textAlign = TextAlign.Center,
+                )
+            }
+
+            Spacer(modifier = Modifier.height(32.dp))
+
+            Text(
+                text = "Powered By",
+                fontSize = 14.sp,
+                fontWeight = FontWeight.SemiBold,
+                textAlign = TextAlign.Center
+            )
+
+            Text(
+                text = "GitHub API",
+                fontSize = 24.sp,
+                fontWeight = FontWeight.Black,
+                textAlign = TextAlign.Center
+            )
+
+            Spacer(modifier = Modifier.height(24.dp))
+        }
+    }
 }
