0d41b7fea5dab000dda5e2abf84f5e1b90f3ed98
Victor Kabata <39780120+VictorKabata@users.noreply.github.com>
2022-07-13 22:28:25 +0300

    Setup realm db

--- settings.gradle.kts
+++ settings.gradle.kts
@@ -1,3 +1,11 @@
+pluginManagement {
+    repositories {
+        gradlePluginPortal()
+        mavenCentral()
+        maven(url = "https://maven.pkg.jetbrains.space/public/p/compose/dev")
+    }
+}
+
 rootProject.name = "Gistagram"
 
 include(":shared", ":androidApp", "desktopApp")
