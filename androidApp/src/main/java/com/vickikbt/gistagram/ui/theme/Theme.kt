456254a2d61ee2351e7412a3784dad90bbdc7577
Kagiri Charles <59829833+Kagiri11@users.noreply.github.com>
2022-07-15 22:39:40 +0300

    made fixes for color changing

--- androidApp/src/main/java/com/vickikbt/gistagram/ui/theme/Theme.kt
+++ androidApp/src/main/java/com/vickikbt/gistagram/ui/theme/Theme.kt
@@ -27,9 +27,7 @@ private val LightColorPalette = lightColors(
 
 @Composable
 fun GistagramTheme(darkTheme: Boolean = isSystemInDarkTheme(), content: @Composable() () -> Unit) {
-
     val colors = if (darkTheme) DarkColorPalette else LightColorPalette
-
     MaterialTheme(
         colors = colors,
         typography = Typography,
