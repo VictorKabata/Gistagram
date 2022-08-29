de1b5100948f363d838928620a2115a9568674a4
ndiritumichael <ndiritumichael5@gmail.com>
2022-08-29 22:20:45 +0300

    add iosApp template

---
+++ iosGistagram/iosGistagram/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  iosGistagram
+//
+//  Created by Michael Ndiritu on 29/08/2022.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ iosGistagram/iosGistagram/iosGistagramApp.swift
@@ -0,0 +1,17 @@
+//
+//  iosGistagramApp.swift
+//  iosGistagram
+//
+//  Created by Michael Ndiritu on 29/08/2022.
+//
+
+import SwiftUI
+
+@main
+struct iosGistagramApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
