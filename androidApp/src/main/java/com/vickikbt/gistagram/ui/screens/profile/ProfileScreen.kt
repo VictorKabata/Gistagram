a3f2568aa1a7aa3c709f1a31fe80c5551683073a
Victor Kabata <39780120+VictorKabata@users.noreply.github.com>
2022-08-31 02:05:07 +0300

    Update ProfileScreen.kt

--- androidApp/src/main/java/com/vickikbt/gistagram/ui/screens/profile/ProfileScreen.kt
+++ androidApp/src/main/java/com/vickikbt/gistagram/ui/screens/profile/ProfileScreen.kt
@@ -10,10 +10,7 @@ import androidx.compose.foundation.lazy.LazyRow
 import androidx.compose.foundation.lazy.items
 import androidx.compose.foundation.shape.CircleShape
 import androidx.compose.foundation.shape.RoundedCornerShape
-import androidx.compose.material.Button
-import androidx.compose.material.ExperimentalMaterialApi
-import androidx.compose.material.MaterialTheme
-import androidx.compose.material.Text
+import androidx.compose.material.*
 import androidx.compose.runtime.*
 import androidx.compose.runtime.livedata.observeAsState
 import androidx.compose.ui.Alignment
